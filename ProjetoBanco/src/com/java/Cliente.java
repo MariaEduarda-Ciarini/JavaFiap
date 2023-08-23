cfc9d9c5fda16401ec158532031f42e9e968261a
Maria Eduarda Ciarini Moura <DudaCiarinii@gmail.com>
2023-08-23 17:06:24 -0300

    aprendendo sobre
    Classe, métodos, atributos

---
+++ ProjetoBanco/src/com/java/Cliente.java
@@ -0,0 +1,9 @@
+package com.java;
+
+public class Cliente {
+	
+	String nome;
+	byte idade;
+	
+
+}
---
+++ ProjetoBanco/src/com/java/Conta.java
@@ -0,0 +1,44 @@
+ package com.java;
+
+public class Conta {
+	
+	private int numero;
+	private double saldo;
+	
+	public Conta() {
+		
+	}
+	
+	public Conta(int numero) {
+		this.numero = numero;
+		this.saldo = 0;
+	}
+	
+	public Conta(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public Conta(int numero, double saldo) {
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+	
+	public double recuperarSaldo() {
+		return saldo;
+	}
+	
+	public void depositar(double valor) {
+		saldo = saldo + valor;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+
+
+}
