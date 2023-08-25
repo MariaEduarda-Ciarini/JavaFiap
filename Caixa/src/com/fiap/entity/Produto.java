d907edfc35eaa89deea69dd0fadf222ae9df2d00
Maria Eduarda Ciarini Moura <DudaCiarinii@gmail.com>
2023-08-24 21:43:40 -0300

    java arrays
    oo

---
+++ Caixa/src/com/fiap/entity/Produto.java
@@ -0,0 +1,33 @@
+package com.fiap.entity;
+
+public class Produto {
+		
+		String nome;
+		String descricao;
+		float valor;
+		
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getDescricao() {
+			return descricao;
+		}
+		public void setDescricao (String descricao) {
+			this.descricao = descricao;
+		}
+		public float getValor() {
+			return valor;
+		}
+		public void setValor(float valor) {
+		    this.valor = valor;
+		}
+		
+		@Override
+		public String toString() {
+			return "Produto [nome=" + nome + ", descricao=" + descricao + ", valor=" + valor + "]";
+		}
+		
+}
